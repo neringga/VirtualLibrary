0ea3ad137e4a4f927bd1dbaed905d356433a9be6
neringga <geigalaite@gmail.com>
2018-09-16 23:52:58 +0300

    Registration (#1)
    * Added FaceDetection and EmguCV
    
    * Solution with registration form and face detection prototype
    
    * Deleted unnessesary folders

---
+++ VirtualLibrary/FaceDetection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+public class FaceDetection
+{
+	public FaceDetection()
+	{
+
+    }
+
+    public static Image<Bgr, Byte> Detect(Image<Bgr, Byte> nextFrame, CascadeClassifier cascade)
+    {
+        Image<Gray, byte> grayframe = nextFrame.Convert<Gray, byte>();
+        var faces = cascade.DetectMultiScale(grayframe, 1.2, 3);
+
+        foreach (var face in faces)
+        {
+            nextFrame.Draw(face, new Bgr(Color.Cyan), 3);
+        }
+        return nextFrame;
+    }
+}
