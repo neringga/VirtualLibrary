a4df975a600a42cca1368879bdcac2d616cdadaf
Viktorija <viktorija.kondratjevaite@gmail.com>
2018-10-07 10:34:37 +0300

    added Translation and LanguageValuePair classes

---
+++ VirtualLibrary/Localization/Translation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrary.Localization
+{
+    public class Translation
+    {
+        public string id;
+        public List<LanguageValuePair> translatedStrings;
+    }
+}
