f2bb6dd693eced00bb7ef3db3e389f2f85017fbd
Gytis Čepkauskas <gytiscep98@gmail.com>
2018-10-08 16:05:34 +0300

    New interface, renamed several const

---
+++ VirtualLibrary/IEmguCvFaceRecognition.cs
@@ -0,0 +1,17 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualLibrary.View
+{
+    interface IEmguCvFaceRecognition
+    {
+        string Recognize(Image<Bgr, Byte> cameraDisplay);
+
+        void AddUser(List<Image<Gray, byte>> trainingSet, string name);
+    }
+}
