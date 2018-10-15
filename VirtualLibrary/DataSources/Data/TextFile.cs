f835efc2359d987593fc293911db7c6ebddc3d3a
Gytis Čepkauskas <gytiscep98@gmail.com>
2018-10-15 19:06:45 +0300

    quick fix for a lecture pt2

--- VirtualLibrary/DataSources/Data/TextFile.cs
+++ VirtualLibrary/DataSources/Data/TextFile.cs
@@ -7,15 +7,12 @@ namespace VirtualLibrary.DataSources.Data
     {
         public List<string> ReadTextFile(string fileName)
         {
+            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+            var file = new StreamReader(Path.Combine(directoryInfo.FullName, fileName));
+
             var textList = new List<string>();
-            var directoryInfo = Directory.GetParent(
-                Directory.GetCurrentDirectory()).Parent;
-            if (directoryInfo != null)
-            {
-                var file = new StreamReader(Path.Combine(directoryInfo.FullName, fileName));
-                string line;
-                while ((line = file.ReadLine()) != null) textList.Add(line);
-            }
+            string line;
+            while ((line = file.ReadLine()) != null) textList.Add(line);
 
             return textList;
         }
