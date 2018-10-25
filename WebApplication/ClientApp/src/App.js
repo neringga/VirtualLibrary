be00e83f07ba31294d87a98a15e4ea19bfe893f2
neringga <geigalaite@gmail.com>
2018-10-26 00:04:39 +0300

    Added open page and registration fields

--- WebApplication/ClientApp/src/App.js
+++ WebApplication/ClientApp/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
+import { Switch, Route } from 'react-router';
+
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Registration } from './components/Registration';
 
 export default class App extends Component {
-  displayName = App.name
 
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
-    );
-  }
+	render() {
+		return (
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/registration' component={Registration} />
+			</Switch>
+		);
+	}
 }
---
+++ WebApplication/ClientApp/src/components/Registration.jsx
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react';
+import "./Registration.css";
+
+export class Registration extends Component {
+
+
+	render() {
+		return (
+			<div className="container">
+				<div className="box">
+					<div className="registration">
+						<h1>Register</h1>
+						<input type="text" placeholder="Username" />
+						<input type="text" placeholder="Name" />
+						<input type="text" placeholder="Surname" />
+						<input type="text" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<input type="password" placeholder="Repeat Password" />
+						<button>Register</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
