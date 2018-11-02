3ea53947f256f462de3b43f562b5a1ae52f772dc
Viktorija <viktorija.kondratjevaite@gmail.com>
2018-11-02 11:01:36 +0200

    added input validation for signing in

--- WebApplication/ClientApp/src/components/SigningIn.jsx
+++ WebApplication/ClientApp/src/components/SigningIn.jsx
@@ -2,7 +2,9 @@
 import axios from "axios";
 import "./SigningIn.css";
 import {
-    
+    usernameDoesntExist,
+    noUsername,
+    noPassword
 } from "./Constants.jsx";
 import { Link } from "react-router-dom";
 
@@ -14,4 +16,26 @@ export class SigningIn extends Component {
             password: "",
         };
     }
+
+    handleInputChange = event => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    checkInput = event => {
+        if (this.state.username == null) {
+            alert(noUsername);
+            return false;
+        }
+        if (
+            this.state.password == null ) {
+            alert(noPassword);
+            return false;
+        }
+        return true;
+    };
+
+
 }
