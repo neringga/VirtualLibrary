8a35024d6031ee8e566cf6e391934a119a1fd2f7
Viktorija <viktorija.kondratjevaite@gmail.com>
2018-11-18 11:39:12 +0200

    add Lazy initialization to BookList and fix configuration reguest path

--- WebApplication/ClientApp/src/components/BookList.jsx
+++ WebApplication/ClientApp/src/components/BookList.jsx
@@ -14,7 +14,7 @@ export class BookList extends Component {
 
   componentDidMount() {
     axios
-      .get(HttpRequestPath + ConfigurationManager.AppSettings["BookListApi"])
+        .get(HttpRequestPath + '<%=ConfigurationManager.AppSettings["BookListApi"] %>')
       .then(response => {
         this.setState({
           books: response.data
