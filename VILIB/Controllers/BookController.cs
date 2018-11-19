c138e92adc6d49bba7f62a50e69cf4a438170efa
neringga <geigalaite@gmail.com>
2018-11-19 11:43:59 +0200

    Implemented book returning

--- VILIB/Controllers/BookController.cs
+++ VILIB/Controllers/BookController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using VILIB.DataSources.Data;
 using VILIB.Helpers;
 using VILIB.Model;
 using VILIB.Presenters;
@@ -36,7 +37,7 @@ namespace VILIB.Controllers
             };
         }
 
-        
+
 
         public async Task<HttpResponseMessage> Put()
         {
@@ -44,7 +45,7 @@ namespace VILIB.Controllers
             string jsonContent = await requestContent.ReadAsStringAsync();
             var book = JsonConvert.DeserializeObject<Book>(jsonContent);
 
-            if (!book.IsTaken)
+            if (!book.IsTaken) //TODO book check
             {
                 var takenBook = _takenBookPresenter.AddTakenBook((IBook)book, "ner"); //TODO user authentification
                 return JsonResponse.JsonHttpResponse<Object>(takenBook.HasToBeReturned);
@@ -77,5 +78,36 @@ namespace VILIB.Controllers
 
     }
 
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ReturnBookController : ApiController
+    {
+        private TakenBookPresenter _takenBookPresenter;
+
+        public ReturnBookController(TakenBookPresenter takenBookPresenter)
+        {
+            _takenBookPresenter = takenBookPresenter;
+        }
+
+        public async Task<HttpResponseMessage> Put()
+        {
+            HttpContent requestContent = Request.Content;
+            string jsonContent = await requestContent.ReadAsStringAsync();
+            var bookCode = JsonConvert.DeserializeObject<String>(jsonContent);
+            StaticDataSource.CurrUser = "ner"; //TODO user authentification
+            try
+            {
+                var takenBook = _takenBookPresenter.FindTakenBookByCode(bookCode,
+                    StaticDataSource.CurrUser);
+                _takenBookPresenter.RemoveTakenBook(takenBook);
+                return JsonResponse.JsonHttpResponse<Object>(true);
+            }
+            catch (Exception)
+            {
+                return JsonResponse.JsonHttpResponse<Object>(false);
+            }
+
+        }
+
+    }
 
 }
