34d2b4ce149fac60ce2254ff9fc4165956f978b5
Viktorija <viktorija.kondratjevaite@gmail.com>
2018-12-11 13:32:25 +0200

    translated strings in Start.jsx

--- VILIB/DataSources/DbDataSource.cs
+++ VILIB/DataSources/DbDataSource.cs
@@ -144,7 +144,7 @@ namespace VILIB.DataSources.Data
         {
             //TODO
             return await _dbContext.SaveChangesAsync();
-
+        }
         public IList<string> GetHashtagList()
         {
             return _dbContext.Hashtags.Select(g => g.Hastag).ToList();
