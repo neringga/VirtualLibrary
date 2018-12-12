f08f0873b0763d2e855575c0643f31d54fa94e1e
neringga <geigalaite@gmail.com>
2018-12-12 12:50:55 +0200

    migrations

---
+++ Database/Migrations/201812120920460_FaceImage.cs
@@ -0,0 +1,26 @@
+namespace Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FaceImage : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.DbFaceImages",
+                c => new
+                    {
+                        RowId = c.Int(nullable: false, identity: true),
+                        Nickname = c.String(),
+                        Bytes = c.Binary(),
+                    })
+                .PrimaryKey(t => t.RowId);
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.DbFaceImages");
+        }
+    }
+}
